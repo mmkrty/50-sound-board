d34dbe8c65ea0d3f13b0e4609fd9bba76f3ef484
Hao Chen <mmkrty@gmail.com>
2021-03-03 21:45:45 +0100

    complete styling and js function.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"]
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+// const sounds1 = document.querySelectorAll("audio");
+// console.log(sounds1);
 
 sounds.forEach(sound =>{
     const btn = document.createElement("button");
     btn.classList.add("btn");
     btn.innerText = sound;
+   
+
     document.getElementById("buttons").appendChild(btn);
+
+    btn.addEventListener("click", ()=>{
+        stopSound();
+        document.getElementById(sound).play();
+    })
 })
+
+function stopSound(){
+    sounds.forEach(sound =>{
+        const song = document.getElementById(sound);
+        song.pause();
+        song.currentTime = 0;
+    })
+}
